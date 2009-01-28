ea185b4ddeb50f7fc05b14ecd5393dfd37c31364
dweiss <dweiss@7e8c87aa-b7f8-0310-9a37-b181ab0e442b>
2009-01-28 08:54:25 +0000

    README file updated, added corba code to the distribution.
    git-svn-id: https://ophelia.cs.put.poznan.pl/svn/prv-dweiss@3776 7e8c87aa-b7f8-0310-9a37-b181ab0e442b

--- dyna-sockets/src/scripts/jdyna.cmd
+++ dyna-sockets/src/scripts/jdyna.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET DYNA_JAR="%~dp0jdyna-sockets.jar;~dp0player.jar"
+SET DYNA_JAR="%~dp0jdyna-sockets.jar;%~dp0jdyna-corba.jar;%~dp0player.jar;%CLASSPATH%"
 
 SET COMMAND=%1
 SET ARGS=
